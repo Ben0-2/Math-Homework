db4290680acde7c45f1d9fd19178b83acfb5e1e7
Ben Singer <bensinger2005@gmail.com>
2018-02-23 12:26:08 -0800

    did it

--- src/ConjectureDisproof.java
+++ src/ConjectureDisproof.java
@@ -1,56 +1,73 @@
+import java.util.ArrayList;
 
 public class ConjectureDisproof {
 	public static void main(String[] args) {
-		ConjectureDisproof thing = new ConjectureDisproof() ;
+		ConjectureDisproof thing = new ConjectureDisproof();
 		thing.forLoopConjecture();
+		
 	}
+
 	void forLoopConjecture() {
-		for(int i=1000; i<10000; i++) {
-			if(i%2 == 1&& !isPrime(i)) {
-				for(int x = 0; x<i/2 +1/2; x++) {
-					if(isSquare(x)) {
-						for(int j = 0; j<i/2 + 1; j++) {
-							if(isPrime(j)) {
-								if((2*x + j) == i) {
-									
-									System.out.println(i);
-								}
-							}
-						}
+		ArrayList<Integer> squareList = new ArrayList<>();
+		ArrayList<Boolean> primeList = new ArrayList<>();
+		for (int i = 0; i < 10000; i++) {
+			if (isSquare(i)) {
+				squareList.add(i);
+				
+			}
+			primeList.add(isPrime(i));
+
+		}
+		System.out.println(squareList);
+		System.out.println(primeList);
+		for (int j = 1001; j < 10000; j += 2) {
+			boolean foundSum = false;
+			for (int k : squareList) {
+				if (k < j) {
+					int p = j - k;
+					if (primeList.get(p)) {
+						foundSum = true;
 					}
+				}
+
+			}
+			if (!foundSum) {
+				System.out.println(j);
+			}
+		}
+	}
+
+	boolean isSquare(int a) {
+		if (a == 2) {
+			return true;
+		}
+
+		else {
+			for (int i = 0; i < a / 2; i++) {
+				if (a == 2 * i * i) {
+					return true;
 				}
 			}
 		}
-	}boolean isSquare(int a) {
-		if(a==1) {
-			return true;
-		}
-		else {
-		for(int i=0;i<a;  i++) {
-			if(a==i*i) {
-				return true;
-			}
-		}
-		}
 
 		return false;
-		
-	}
-	boolean isPrime(int a) {
-		for(int i=2; i<a; i++) {
-			if(a%i == 0) {
-				boolean b = false;
-				return b;
-				
-			}
-			
 
+	}
+
+	boolean isPrime(int a) {
+		if (a == 0 || a == 1) {
+			return false;
+		} else {
+			for (int i = 2; i < a; i++) {
+				if (a % i == 0) {
+					boolean b = false;
+					return b;
+
+				}
+
+			}
 		}
 		return true;
 
-
-
-
-
-		}
+	}
 }
