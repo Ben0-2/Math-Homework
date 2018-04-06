50051412a6803db9cea15545c53d41782833b767
Ben Singer <bensinger2005@gmail.com>
2018-04-06 12:54:59 -0700

    Some Changes

--- src/DoesIExist.java
+++ src/DoesIExist.java
@@ -1,35 +1,42 @@
 import java.util.ArrayList;
 
 public class DoesIExist {
-public DoesIExist(int n) {
-makeZ(n);
-System.out.println(verifyExistence(n));
-}
-public static void main(String[] args) {
-	PF p = new PF(1000);
-	
-	for(int i = 1; i<1000; i++) {
-		if(PF.getPrimeornot().get(i)) {
-			DoesIExist d = new DoesIExist(i);
-			
+	public DoesIExist(int n) {
+		makeZ(n);
+		System.out.println(n + " | " + verifyExistence(n));
+	}
+
+	public static void main(String[] args) {
+	int n = 5010;
+		PF p = new PF(n);
+
+		for (int i = 1; i < n; i++) {
+			if (PF.getPrimeornot().get(i)) {
+				DoesIExist d = new DoesIExist(i);
+
+			}
 		}
 	}
-}
-	public ArrayList<Integer> makeZ(int i){
+
+	public ArrayList<Integer> makeZ(int i) {
 		ArrayList<Integer> list = new ArrayList<>();
-		for(int j = 0; j<i; j++) {
+		for (int j = 0; j < i; j++) {
 			list.add(j);
 		}
 		return list;
-		
+
 	}
-boolean verifyExistence(int i) {
-	for(int j= 1; j<=Math.sqrt(i); j++) {
-		if(j*j == i-1) {
-			return true;
+
+	boolean verifyExistence(int i) {
+		for (int j = 1; j <= i-1; j++) {
+
+			if ((j * j) % i == i - 1) {
+			System.out.println(j+ " is true for " + i + "!");
+				return true;
+			}
+
 		}
+		return false;
+
 	}
-	return false;
-	
-}
 }
